aebbf33056edf84c5709128fb66b393387a031ed
DESKTOP-OQPLT75\A-16 <DESKTOP-OQPLT75\A-16>
2022-08-12 15:10:38 +0800

    Initial Commit - Impement MVC - Implement DI

---
+++ Assets/Scripts/Gameplay/GameplayLauncher.cs
@@ -0,0 +1,37 @@
+using Agate.MVC.Base;
+using Agate.MVC.Core;
+using ExampleGame.Boot;
+using ExampleGame.Module.ClickGame;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ExampleGame.Gameplay
+{
+	public class GameplayLauncher : SceneLauncher<GameplayLauncher, GameplayView>
+	{
+        private ClickGameController _clickGame;
+        public override string SceneName => "Gameplay";
+
+        protected override IConnector[] GetSceneConnectors()
+        {
+            return null;
+        }
+
+        protected override IController[] GetSceneDependencies()
+        {
+            return new IController[]{new ClickGameController()};
+        }
+
+        protected override IEnumerator InitSceneObject()
+        {
+            _clickGame.SetView(_view.ClickGameView);
+            yield return null;
+        }
+
+        protected override IEnumerator LaunchScene()
+        {
+            yield return null;
+        }
+    }
+}
